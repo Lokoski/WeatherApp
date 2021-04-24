9ebd9c67f2786ba5462e938faa6c4fd5e4776091
Alex <aleksandar.lokoski@gmail.com>
2021-04-24 16:07:50 -0400

    fixed fetch

--- src/api/fetchWeather.jsx
+++ src/api/fetchWeather.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const URL = 'http://api.openweathermap.org/data/2.5/weather'
+const URL = 'https://api.openweathermap.org/data/2.5/weather'
 const API_KEY = '64cf36f2a1707d14cf687c91b2d4e591';
 
 export const fetchWeather = async (query) => {
