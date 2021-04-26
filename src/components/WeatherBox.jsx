05f80ea7bd7e076ea42b62e1ea157740bec58674
Alex <aleksandar.lokoski@gmail.com>
2021-04-25 21:57:14 -0400

    cleaned up code

--- src/components/WeatherBox.jsx
+++ src/components/WeatherBox.jsx
@@ -1,88 +1,48 @@
 import React, { useState } from "react";
 import WeatherInfo from "./WeatherInfo";
-import { fetchWeather } from "../api/fetchWeather.jsx"
+import { fetchWeather } from "../api/fetchWeather.jsx";
 
 function WeatherBox() {
-//   const API_KEY = "64cf36f2a1707d14cf687c91b2d4e591"
-//   const [search, setSearch] = useState();
-//   const [weatherData, setWeatherData] = useState({
-//       temp: null,
-//       desc: null,
-//       city: null,
-//       tempMax: null,
-//       tempMin: null
-//   });
+  const [query, setQuery] = useState("");
+  const [weather, setWeather] = useState({
+    temp: null
+  });
 
-//   function updateSearch(e) {
-//     setSearch(e.target.value);
-//   }
+  const searchWeather = async (e) => {
+    if (e.key === "Enter") {
+      const data = await fetchWeather(query);
 
-
-
-//   function getWeatherData(){
-//       fetch(`https://api.openweathermap.org/data/2.5/weather?q=${search},us&appid=${API_KEY}
-//       `)
-//       .then(response => response.json())
-//       .then(data => setWeatherData({
-//           temp: toFar(data.main.temp),
-//           desc: data.weather[0].main,
-//           city: data.name,
-//           tempMax:toFar(data.main.temp_max),
-//           tempMin: toFar(data.main.temp_min),
-//           feelsLike: toFar(data.main.feels_like)
-//       }))
-      
-//   }
-
-const [query, setQuery] = useState('');
-const [weather, setWeather] = useState({
-  temp: null,
-  desc: null,
-  city: null,
-  tempMax: null,
-  tempMin: null,
-  feelsLike: null,
-  country: null,
-  icon: null,
-});
-
-const searchWeather = async (e) => {
-    if(e.key === 'Enter') {
-        const data = await fetchWeather(query);
-
-        setWeather({
-          temp: toFar(data.main.temp),
-          desc: data.weather[0].main,
-          city: data.name,
-          tempMax:toFar(data.main.temp_max),
-          tempMin: toFar(data.main.temp_min),
-          feelsLike: toFar(data.main.feels_like),
-          country: data.sys.country,
-          icon: data.weather[0].icon
-        });
-        setQuery('')
+      setWeather({
+        temp: toFar(data.main.temp),
+        desc: data.weather[0].main,
+        city: data.name,
+        tempMax: toFar(data.main.temp_max),
+        tempMin: toFar(data.main.temp_min),
+        feelsLike: toFar(data.main.feels_like),
+        country: data.sys.country,
+        icon: data.weather[0].icon,
+      });
+      setQuery("");
     }
-}
+  };
 
-const searchWeatherButton = async (e) => {
-        const data = await fetchWeather(query);
-        setWeather({
-          temp: toFar(data.main.temp),
-          desc: data.weather[0].main,
-          city: data.name,
-          tempMax:toFar(data.main.temp_max),
-          tempMin: toFar(data.main.temp_min),
-          feelsLike: toFar(data.main.feels_like),
-          country: data.sys.country,
-          icon: data.weather[0].icon
-        });
-        setQuery('')
-}
+  const searchWeatherButton = async (e) => {
+    const data = await fetchWeather(query);
+    setWeather({
+      temp: toFar(data.main.temp),
+      desc: data.weather[0].main,
+      city: data.name,
+      tempMax: toFar(data.main.temp_max),
+      tempMin: toFar(data.main.temp_min),
+      feelsLike: toFar(data.main.feels_like),
+      country: data.sys.country,
+      icon: data.weather[0].icon
+    });
+    setQuery("");
+  };
 
-
-
-  function toFar(temp){
-      return ((temp - 273.14) * (9.0 / 5.0) + 32).toFixed(0);
+  function toFar(temp) {
+    return ((temp - 273.14) * (9.0 / 5.0) + 32).toFixed(0);
   }
 
   return (
@@ -93,16 +53,23 @@ const searchWeatherButton = async (e) => {
           <input
             placeholder="Search..."
             className="search-input"
-            onChange={e => setQuery(e.target.value)}
+            onChange={(e) => setQuery(e.target.value)}
             value={query}
             onKeyPress={searchWeather}
           />
-          <button className="material-icons" onClick={searchWeatherButton}>search</button>
+          <button className="material-icons" onClick={searchWeatherButton}>
+            search
+          </button>
         </div>
       </header>
-      {/* <p className="error">Invalid city</p> */}
       <section className="weather-info">
-            {weather.temp === null ? (<p>Please enter a city name<i className="material-icons">wb_sunny</i></p>) : <WeatherInfo weather={weather}/>}
+        {weather.temp === null ? (
+          <p>
+            Please enter a city name<i className="material-icons">wb_sunny</i>
+          </p>
+        ) : (
+          <WeatherInfo weather={weather} />
+        )}
       </section>
     </div>
   );
--- src/components/WeatherInfo.jsx
+++ src/components/WeatherInfo.jsx
@@ -1,19 +1,22 @@
-import React from 'react'
+import React from "react";
 
 function WeatherInfo(props) {
-    const { temp, city, desc, tempMin, tempMax,country, icon} = props.weather
-    return (
-        <>
-            <div className="weather-desc">
-                <h3>Location</h3>
-                <p>{city} / {country}</p>
-                
-            </div>
-            <div className="weather-desc">
-                <h3>Temperature</h3>
-                <p>{temp} <span className="symbol"></span>F </p>
-            </div>
-            {/* <div className="weather-desc">
+  const { temp, city, desc, tempMin, tempMax, country, icon } = props.weather;
+  return (
+    <>
+      <div className="weather-desc">
+        <h3>Location</h3>
+        <p>
+          {city} / {country}
+        </p>
+      </div>
+      <div className="weather-desc">
+        <h3>Temperature</h3>
+        <p>
+          {temp} <span className="symbol"></span>F{" "}
+        </p>
+      </div>
+      {/* <div className="weather-desc">
                 <h3>Todays Minimum</h3>
                 <p>{tempMin} <span className="symbol"></span>F</p>
             </div>
@@ -21,15 +24,22 @@ function WeatherInfo(props) {
                 <h3>Todays Maximum</h3>
                 <p>{tempMax} <span className="symbol">F</span></p>
             </div> */}
-            <div className="weather-desc">
-                <h3>{desc}</h3>
-              <img src={`https://openweathermap.org/img/wn/${icon}@2x.png`} alt="icon"/>
-            </div>
-            <div className="weather-desc">
-                <h4>Min: {tempMin} <span className="symbol"></span>F  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Max: {tempMax} <span className="symbol"></span>F </h4>
-            </div>
-        </>
-    )
+      <div className="weather-desc">
+        <h3>{desc}</h3>
+        <img
+          src={`https://openweathermap.org/img/wn/${icon}@2x.png`}
+          alt="icon"
+        />
+      </div>
+      <div className="weather-desc">
+        <h4>
+          Min: {tempMin} <span className="symbol"></span>F
+          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Max: {tempMax}{" "}
+          <span className="symbol"></span>F{" "}
+        </h4>
+      </div>
+    </>
+  );
 }
 
-export default WeatherInfo
+export default WeatherInfo;
